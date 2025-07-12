69ce848ce3fdc7c528588b6b01486fa191722af2
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-07-12 21:33:33 +0800

    feat: 移除郑码支持

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,6 @@ import { Bot, webhookCallback } from 'grammy';
 import xiaohe from './msgHandlers/xiaohe';
 import hc from './msgHandlers/hc';
 import cangjie from './msgHandlers/cangjie';
-import zhengma from './msgHandlers/zhengma';
 import wubi86 from './msgHandlers/wubi86';
 import wubi98 from './msgHandlers/wubi98';
 
@@ -23,7 +22,6 @@ bot.command('help', async ctx => {
   const help = `- 小鹤音形 xiaohe xh xhyx *小鹤
 - hc hc *hc
 - 仓颉 cangjie cj *仓颉
-- 郑码 zhengma zheng zm *郑
 - 五笔86 wubi86 wb86 *五笔86
 - 五笔98 wubi98 wb98 *五笔98`;
   await ctx.reply(help, {
@@ -78,21 +76,6 @@ bot.command(['cangjie', 'cj'], async ctx => {
   }
 });
 
-bot.command(['zhengma', 'zm', 'zheng'], async ctx => {
-  const message = ctx.msg.text;
-  const result = await zhengma(message);
-
-  try{
-    await ctx.reply(result.join(''), {
-      reply_parameters: {
-        message_id: ctx.msgId,
-      },
-    });
-  } catch{
-    bot.api.sendMessage(ctx.chatId, '总之就是我不知道你在说什么喵！');
-  }
-});
-
 bot.command(['wubi86', 'wb86'], async ctx => {
   const message = ctx.msg.text;
   const result = await wubi86(message);
@@ -130,7 +113,6 @@ bot.on('message', async ctx => {
   if(message.startsWith('*小鹤')) result = await xiaohe(message);
   else if(message.startsWith('*hc')) result = await hc(message);
   else if(message.startsWith('*仓颉')) result = await cangjie(message);
-  else if(message.startsWith('*郑')) result = await zhengma(message);
   else if(message.startsWith('*五笔86')) result = await wubi86(message);
   else if(message.startsWith('*五笔98')) result = await wubi98(message);
   else return;
